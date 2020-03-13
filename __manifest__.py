0096925d61e57f5ef4e1ed91558354c144636cfe
nizar <boutitinizar@gmail.com>
2020-03-13 13:21:21 +0100

    Initial commit

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "hannon_amount_to_letter",
+    'author': "HannonIT",
+    'website': "https://hannon.tn",
+
+
+    'category': 'Accounting',
+    'version': '0.1',
+
+
+    'depends': ['base', 'account'],
+
+    'data': [
+        'views/views.xml',
+    ]
+}
